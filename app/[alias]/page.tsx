d06822cacbc7599b2979af86c1aa2c259124a383
Harrydmyh <harrydm@bu.edu>
2025-11-18 23:05:15 -0500

    Added debugging

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -3,6 +3,7 @@ import {redirect} from "next/navigation";
 
 export default async function RetrieveURL({params}: { params: { alias: string } }) {
     const alias = params.alias || '';
+    console.log(alias);
     const url = await findAlias(alias)
     if (url) {
         redirect(url)
